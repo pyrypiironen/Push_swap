656bfbc22e6889e70a4ccc7aa04e78d1ba968fd5
Pyry-peetri Piironen <ppiirone@c2r4p7.hive.fi>
2022-08-02 19:05:04 +0300

    Move print_stacks to visualizer and modified it.

--- srcs/swap/main.c
+++ srcs/swap/main.c
@@ -85,34 +85,3 @@ void	ps_error(t_data	*d)
 	d->visual = 100;// (this line is for silence the error message)
 	exit(-1);
 }
-
-void	print_stacks(t_data *d, t_stack **a, t_stack **b) // Delete in the end
-{
-	*a = d->head_a;
-	*b = d->head_b;
-	int a_end = 0;
-	int b_end = 0;
-
-	while (a_end == 0 | b_end == 0)
-	{
-		if (d->head_a != NULL && (*a)->value != '\0' && a_end == 0)
-			ft_printf("{green}%15d", (*a)->value);
-		else
-			ft_printf("%15s", "");
-		if (d->head_b != NULL && (*b)->value != '\0' && b_end == 0)
-			ft_printf("{yellow}%15d", (*b)->value);
-		if (d->head_a != NULL && (*a)->next)
-			*a = (*a)->next;
-		else
-			a_end = 1;
-		if (d->head_b != NULL && (*b)->next)
-			*b = (*b)->next;
-		else
-			b_end = 1;
-		ft_printf("\n");
-	}
-	ft_printf("{green}%15s", "_");
-	ft_printf("{yellow}%15s\n", "_");
-	ft_printf("{green}%15s", "a");
-	ft_printf("{yellow}%15s\n", "b");
-}
--- srcs/visualizer/visualizer.c
+++ srcs/visualizer/visualizer.c
@@ -12,10 +12,43 @@
 
 # include "../../includes/push_swap.h"
 
-void	visualizer(t_data *d, t_stack **a, t_stack **b)
+void	print_stacks(t_data *d, t_stack **a, t_stack **b) // Delete in the end
 {
+	system("clear");
+
 	*a = d->head_a;
 	*b = d->head_b;
+	int a_end = 0;
+	int b_end = 0;
 
-
+	while (a_end == 0 | b_end == 0)
+	{
+		if (d->head_a != NULL && (*a)->value != '\0' && a_end == 0)
+			ft_printf("{green}%15d", (*a)->value);
+		else
+			ft_printf("%15s", "");
+		if (d->head_b != NULL && (*b)->value != '\0' && b_end == 0)
+			ft_printf("{yellow}%15d", (*b)->value);
+		if (d->head_a != NULL && (*a)->next)
+			*a = (*a)->next;
+		else
+			a_end = 1;
+		if (d->head_b != NULL && (*b)->next)
+			*b = (*b)->next;
+		else
+			b_end = 1;
+		ft_printf("\n");
+	}
+	ft_printf("{green}%15s", "_");
+	ft_printf("{yellow}%15s\n", "_");
+	ft_printf("{green}%15s", "a");
+	ft_printf("{yellow}%15s\n", "b");
+	sleep(1);
 }
+// void	visualizer(t_data *d, t_stack **a, t_stack **b)
+// {
+// 	*a = d->head_a;
+// 	*b = d->head_b;
+
+
+// }
